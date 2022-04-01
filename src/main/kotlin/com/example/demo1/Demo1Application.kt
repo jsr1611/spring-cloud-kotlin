71e6c28f5da6c8a8cfc5a88f1f4d30facb38ee12
Jumanazar <jsr1611@outlook.com>
2022-04-01 13:33:40 +0900

    added rest template

--- src/main/kotlin/com/example/demo1/Demo1Application.kt
+++ src/main/kotlin/com/example/demo1/Demo1Application.kt
@@ -2,19 +2,35 @@ package com.example.demo1
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-class Demo1Application
+class Demo1Application{
+
+    @Bean
+    fun restTemplate() = RestTemplate()
+}
 
 fun main(args: Array<String>) {
     runApplication<Demo1Application>(*args)
 }
 
 @RestController
-class TestController{
+class TestController(val restTemplate: RestTemplate){
+
+
 
     @GetMapping
-    fun hello() = "Hello Kotlin World!"
+    fun hello(): String{
+        // var responseFromDemo2
+        // var result = "Hello From Demo1" + responseFromDemo2
+        // return result
+
+        val responseFromDemo2 = restTemplate.getForObject("http://localhost:8082", String::class.java)
+        val result = "Hello Kotlin World from Demo1 " + responseFromDemo2
+        return result
+    }
 }
